0a0c6412c9bc622def5455e167f0a6215a3a12ed
yeh-sudo <yehchanshen@gmail.com>
2021-12-20 16:27:10 +0800

    New ground spawner

--- game/Assets/Scripts/tileSpawner.cs
+++ game/Assets/Scripts/tileSpawner.cs
@@ -1,38 +1,64 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class tileSpawner : MonoBehaviour{
-    public GameObject tileToSpawn;
-    public GameObject referenceObject;
-    public float timeOffset = 0.4f;
-    public float distanceBetweenTiles = 0.1F;
-    public float randomValue = 0.8f;
-    private Vector3 previousTilePosition;
-    private float startTime;
-    private Vector3 direction, mainDirection = new Vector3(0, 0, 1), otherDirection = new Vector3(1, 0, 0);
 
-    // Start is called before the first frame update
-    void Start() {
-        previousTilePosition = referenceObject.transform.position;
-        startTime = Time.time;
+    public GameObject groundTile;
+    Vector3 nextSpawnerPoint;
+    private System.Random crand = new System.Random();
+    private int straight = 5;
+    private int left = 0;
+    private int right = 0;
+
+    private int randint() {
+        if (straight != 0) {
+            straight--;
+            int ishole = crand.Next(1, 9);
+            if (ishole == 8) {
+                return 4;
+            }
+            return 1;
+        }
+        if (right != 0) {
+            right--;
+            int ishole = crand.Next(1, 9);
+            if (ishole == 8) {
+                return 5;
+            }
+            return 3;
+        }
+        if (left != 0) {
+            left--;
+            int ishole = crand.Next(1, 9);
+            if (ishole == 8) {
+                return 6;
+            }
+            return 2;
+        }
+        int tmp = crand.Next(1, 15);
+        if (tmp <= 10 && tmp >= 1) {
+            straight = 5;
+            return 1;
+        }
+        else if (tmp >= 11 && tmp <= 12) {
+            left = 5;
+            return 2;
+        }
+        else {
+            right = 5;
+            return 3;
+        }
     }
 
-    // Update is called once per frame
-    void Update() {
-        if (Time.time - startTime > timeOffset) {
-            if (Random.value < randomValue)
-                direction = mainDirection;
-            else {
-                Vector3 temp = direction;
-                direction = otherDirection;
-                mainDirection = direction;
-                otherDirection = temp;
-            }
-            Vector3 spawnPos = previousTilePosition + distanceBetweenTiles * direction;
-            startTime = Time.time;
-            Instantiate(tileToSpawn, spawnPos, Quaternion.Euler(0, 0, 0));
-            previousTilePosition = spawnPos;
+    public void spawnTile() {
+        GameObject tmp = Instantiate(groundTile, nextSpawnerPoint, Quaternion.identity);
+        nextSpawnerPoint = tmp.transform.GetChild(randint()).transform.position;
+    }
+
+    // Start is called before the first frame update
+    void Start(){
+        for (int i = 0; i < 15; i++) {
+            spawnTile();
         }
     }
 }
