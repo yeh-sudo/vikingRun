85855d4ee75736c84fbcf850fb522c17a6b43f85
yeh-sudo <yehchanshen@gmail.com>
2021-12-21 01:40:23 +0800

    Add coins

---
+++ game/Assets/Scripts/Coin.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour{
+
+    public float turnSpeed = -90f;
+
+    private void OnTriggerEnter(Collider other) {
+
+        if (other.gameObject.GetComponent<Obstacle>() != null) {
+            Destroy(gameObject);
+            return;
+        }
+
+        if (other.gameObject.name != "Ch24_nonPBR@Running Crawl") {
+            return;
+        }
+
+
+        Destroy(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        transform.Rotate(0, turnSpeed * Time.deltaTime, 0);
+    }
+}
