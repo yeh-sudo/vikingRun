e919480a9ac265fad3e58a400ba49178799cf20f
yeh-sudo <yehchanshen@gmail.com>
2021-12-21 01:40:35 +0800

    Add die and coins

--- game/Assets/Scripts/groundTile.cs
+++ game/Assets/Scripts/groundTile.cs
@@ -5,6 +5,7 @@ public class groundTile : MonoBehaviour{
 
     tileSpawner groundSpawner;
     public GameObject obstacle;
+    public GameObject coinPrefab;
     private System.Random crand = new System.Random();
 
     private bool hasObs() {
@@ -21,6 +22,7 @@ public class groundTile : MonoBehaviour{
     void Start(){
         groundSpawner = GameObject.FindObjectOfType<tileSpawner>();
         spawnObs();
+        spawnCoins();
     }
 
     private void OnTriggerExit(Collider other) {
@@ -40,5 +42,27 @@ public class groundTile : MonoBehaviour{
         int obstacleIdx = UnityEngine.Random.Range(7, 10);
         Transform spawnPoint = transform.GetChild(obstacleIdx).transform;
         Instantiate(obstacle, spawnPoint.position, Quaternion.identity, transform);
+    }
+
+    void spawnCoins() {
+        int coinsToSpawn = 1;
+        for (int i = 0; i < coinsToSpawn; i++) {
+            GameObject tmp = Instantiate(coinPrefab, transform);
+            tmp.transform.position = getRndPoint(GetComponent<Collider>());
+        }
+    }
+
+    Vector3 getRndPoint(Collider collider) {
+        Vector3 point = new Vector3(
+            UnityEngine.Random.Range(collider.bounds.min.x, collider.bounds.max.x),
+            UnityEngine.Random.Range(collider.bounds.min.y, collider.bounds.max.y),
+            UnityEngine.Random.Range(collider.bounds.min.z, collider.bounds.max.z)
+        );
+        if (point != collider.ClosestPoint(point)) {
+            point = getRndPoint(collider);
+        }
+
+        point.y = 1;
+        return point;
     }
 }
