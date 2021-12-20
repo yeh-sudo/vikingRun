2269c03afbb1f2652ac17b7b26852c9f9c383e4b
yeh-sudo <yehchanshen@gmail.com>
2021-12-20 23:17:01 +0800

    Generate obstacles

--- game/Assets/Scripts/groundTile.cs
+++ game/Assets/Scripts/groundTile.cs
@@ -1,12 +1,26 @@
 using UnityEngine;
+using System;
 
 public class groundTile : MonoBehaviour{
 
     tileSpawner groundSpawner;
+    public GameObject obstacle;
+    private System.Random crand = new System.Random();
+
+    private bool hasObs() {
+        int tmp = crand.Next(1, 16);
+        if (tmp == 15) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 
     // Start is called before the first frame update
     void Start(){
         groundSpawner = GameObject.FindObjectOfType<tileSpawner>();
+        spawnObs();
     }
 
     private void OnTriggerExit(Collider other) {
@@ -17,5 +31,14 @@ public class groundTile : MonoBehaviour{
     // Update is called once per frame
     void Update(){
         
+    }
+
+    void spawnObs() {
+        if (!hasObs()) {
+            return;
+        }
+        int obstacleIdx = UnityEngine.Random.Range(7, 10);
+        Transform spawnPoint = transform.GetChild(obstacleIdx).transform;
+        Instantiate(obstacle, spawnPoint.position, Quaternion.identity, transform);
     }
 }
